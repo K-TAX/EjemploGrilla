05d46ef22a419924d095b4604abb254066f5d17b
Ivan <ivan.peters.v@gmail.com>
2018-04-17 08:46:27 -0300

    inicial commit

--- EjemploGrilla/Form1.Designer.cs
+++ EjemploGrilla/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(534, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(693, 255);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.SeleccionaAlumno);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(59, 35);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(155, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre Alumno";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 446);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- EjemploGrilla/Form1.cs
+++ EjemploGrilla/Form1.cs
@@ -16,5 +16,152 @@ namespace EjemploGrilla
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Llamamos al metodo
+            CrearGrillaAlumnos();
+
+            //Llamamos al metodo Cargar Listado Alumnos
+            CargarListadoAlumnos();
+
+        }
+
+        //sera de uso interno del formulario,
+        //su visualizacion debe ser private
+        //este metodo se encarga de encapsular la logica
+        // de diseño y creacion de la grilla ( Listado Alumnos )
+        private void CrearGrillaAlumnos() {
+            //tipo va hacer la columna 
+            var nombre = new DataGridViewTextBoxColumn();
+            var apellido = new DataGridViewTextBoxColumn();
+            var curso = new DataGridViewTextBoxColumn();
+
+
+            //grilla se añaden las columnas, por meido de un arreglo
+            //haciendo uso de DAtagRidViewColumn[]
+
+            dataGridView1.Columns.AddRange(
+                new System.Windows.Forms.DataGridViewColumn[] {
+                    nombre,     //Columna 0
+                    apellido,   //Columna 1
+                    curso       //Columna 2
+                    //-.........
+                });
+
+            // Propiedades a cada columna
+            //accediendo por su indice ( comienza desde cero )
+            dataGridView1.Columns[0].Name = "Nombre";
+            dataGridView1.Columns[0].Width = 250;
+            dataGridView1.Columns[0].HeaderText = "Nombre Alumno";
+
+
+            dataGridView1.Columns[1].Name = "Apellido";
+            dataGridView1.Columns[1].Width = 250;
+            dataGridView1.Columns[1].HeaderText = "Apellido Alumno";
+
+            dataGridView1.Columns[2].Name = "Curso";
+            dataGridView1.Columns[2].Width = 150;
+            dataGridView1.Columns[2].HeaderText = "Curso Alumno";
+
+        }
+
+        //los metodos void, son aquellos que no retornan valor.
+        //solo generan una accion en el proceso
+        private void CargarListadoAlumnos() {
+
+           
+            //como el metodo BuscarAlumnos(), retorna un List<con arreglos de string>
+            // debemos crear la variable que recibirá dicho objeto
+            List<string[]> listadoAlumnos = BuscarAlumnos();
+
+
+            //el metodo Add de Rows, puede recibir un objeto
+            // foreach, es  un iterador.
+            //iterará al listao alumnos y por cada valor ( lista )
+            // almacenará su resultsado en la variable de tipo string[] alumno
+            // es decir, cada vuelta de la iteracion, tendra a un alumno
+            //entonces si hay cuatro alumnos, dara cuatro vueltas
+            foreach (string[] elAlumno in listadoAlumnos) {
+
+                //add recibe un objeto string[] 
+                //que en este caso es alumno ( por cada vuelta )
+                dataGridView1.Rows.Add( elAlumno );
+
+            }
+            
+
+
+        }
+
+        //retornara una lista de tipo arreglos de string
+        private List<string[]> BuscarAlumnos() {
+            //CRea una lista que soportará arreglos
+            //List genera una interfaz que implementa el tipo de dato 
+            //expuesto < tipo dato >
+            //new List< tipo de dato >, genera el objeto alumnos
+            // List<string[]> alumnos ( crea un tipo de dato ( que se llamara alumno), de tipo List que soporta arreglos de string)
+            // = new List<string[]>(); crea un objeto ( instancia) que detemrina 
+            //que lo que soportará alumnos sea una instancia de un objeto de tipo 
+            //lista y que soporte arreglos de string
+
+            List<string[]> alumnos = new List<string[]>();
+
+            //Add, agrega una lista a la variable tipo LIST alumnos
+            // espera Add un obejto que tenga la misma caracterista de la definicion
+            // es decir un array de string
+           //new string[]{"le", "paso", "los", "valores", "del", "arreglo"}
+            alumnos.Add(new string[]{ "Bastian", "Olavarria", "segundo año"});
+            alumnos.Add(new string[] { "Gerardo", "Silva", "segundo año" });
+            alumnos.Add(new string[] { "Felipe", "Uribe", "segundo año" });
+            alumnos.Add(new string[] { "Luciano", "Alvares", "segundo año" });
+
+            //para retornar un valor,
+            //se requiere que en la implementacion del metodo,
+            //se indique el tipo de retorno
+            return alumnos;
+
+        }
+
+        private void SeleccionaAlumno(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            // el objeto que recibe como prarmetro "e"
+            //trae informacion del evento que se genero sobre la grilla
+            //e.RowIndex
+            //tiene informacion sobre que row se genero el evento
+            // comenzando la primera fila en cero
+            // y el encabezado es -1
+            if (e.RowIndex >= 0) {// solo capturo clik sobre grilla
+                // captura el valor de la celda cero de la grilla, segun
+                //rowIndex generado, al evento click
+                //dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString()
+                //dataGridView1 = corresponde a al grilla
+                //Rows[] es un arreglo de filas comenzando de cero ( listado de los alumnos , en la grilla)
+                //[ e.RowIndex ], es el numero de la fila la cual se genero el evento click.
+                //Cells[] = corresponde a la celda, que necesitan obtener informacion
+                //Value, como al crear grilla se instancia un objeto, value solo captura la perte del valor de dicho objeto ( para la celda )
+                //toString() convierte en string
+
+                //la variable nombre, almacenara el registro o valor
+                 //que este en la celda cero de la fila cliqueada
+                string nombre = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+                //el valor de la variable nombre
+                //lo asignamos a la caja de texto txtNombre.Text
+                txtNombre.Text = nombre;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
